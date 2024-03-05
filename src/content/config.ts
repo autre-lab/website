2e13e1a47dfa8c2accb039a13032b37ee530d9f3
byhow <by.how@hotmail.com>
2024-03-05 00:30:59 -0800

    fix: upgrade issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
@@ -19,6 +20,7 @@ const blogCollection = defineCollection({
 });
 
 const teamCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
   schema: z.object({
     draft: z.boolean(),
     name: z.string(),
